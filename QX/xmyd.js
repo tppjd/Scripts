5625e7d163f26decf098047dd0f531d387d4f9bf
tppjd <75834960+tppjd@users.noreply.github.com>
2022-03-29 09:28:05 +0800

    Update xmyd.js

--- QX/xmyd.js
+++ QX/xmyd.js
@@ -1,4 +1,14 @@
 /*
+by David
+电报群：https://t.me/china20211029
+boxjs地址： https://cdn.jsdelivr.net/gh/bluesmallpig/scripts/David.boxjs.json
+转载请备注名字，谢谢!
+⚠️ 小米运动 应用商店自行搜索下载
+⚠️ 功能：同步步数到微信和支付宝，默认随机20000-30000步，好处懂的人自然懂
+⚠️ 第一次同步前需要进入APP在第三方接口处绑定微信或者支付宝
+⚠️ 用手机号登录一次即可获取数据
+⚠️ 定时建议一天运行一次即可，最好下午3点以后
+⚠️ 一共1个key: xmydapp  对应青龙变量：XMYDAPP
 小米运动：登陆一次即可获取数据
 TG电报群: 群已私有，入群私聊机器人 @DavidLoveBot
 boxjs: https://cdn.jsdelivr.net/gh/bluesmallpig/scripts/David.boxjs.json
